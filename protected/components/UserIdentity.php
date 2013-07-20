ea7b6cf6e0be47e080bfa2cebfd76688be3a173c
YakovGrischenko <coding@mail.ru>
2013-07-21 00:36:50 +0700

    Изменена функция аутентификации пользователя на чтение данных из БД.

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -17,17 +17,23 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
+		//ищем пользователя по имени
+                $record=Users::model()->findByAttributes(array('u_login'=>$this->username));
+                //если пользователь найден и его пароль совпадает с введенным...
+                if($record===null)
+                    $this->errorCode=self::ERROR_USERNAME_INVALID;
+                else if($record->u_password!==md5($this->password))
+                    $this->errorCode=self::ERROR_PASSWORD_INVALID;
+                else
+                {
+                    //...сохраняем данные пользователя (имя и адрес фида)
+                    // (в принципе, адрес фида можно не сохранять, тогда при импорте игр
+                    // нужно будет выполнить дополнительный запрос)
+                    $this->_id=$record->u_id;
+                    $this->setState('name', $record->u_name);
+                    $this->setState('xml', $record->u_xml);
+                    $this->errorCode=self::ERROR_NONE;
+                }
+                return !$this->errorCode;
 	}
 }
