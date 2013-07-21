9acefaf00edde5af7bb78f22a3c8420eedbc8ecc
YakovGrischenko <coding@mail.ru>
2013-07-21 19:57:04 +0700

    Добавлен метод контроля прав доступа пользавателей

--- protected/controllers/DashboardController.php
+++ protected/controllers/DashboardController.php
@@ -9,20 +9,33 @@ class DashboardController extends Controller
 
 	// -----------------------------------------------------------
 	// Uncomment the following methods and override them if needed
-	/*
+	
 	public function filters()
 	{
 		// return the filter configuration for this controller, e.g.:
 		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
+			'accessControl', // perform access control for CRUD operations
 		);
 	}
+        
+        public function accessRules()
+        {
+            return array(
+                array('allow',
+                    'actions'=>array('login','logout'),
+                    'users'=>array('*'),
+                ),
+                array('allow',
+                    'actions'=>array('index'),
+                    'users'=>array('admin'),
+                ),
+                array('deny',  // deny all users
+                    'users'=>array('*'),
+                ),
+            );
+        }
 
-	public function actions()
+	/*public function actions()
 	{
 		// return external action classes, e.g.:
 		return array(
