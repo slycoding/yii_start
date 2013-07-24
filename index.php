8a8ee2a712de9cf752187446e31c8bce6d286a04
qwe <coqweng@mail.ru>
2013-07-24 10:03:41 +0700

    test3

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//test2
+//test3
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../../../yii-1.1.13.e9e4a0/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
